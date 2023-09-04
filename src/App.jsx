f46b07109049774be9e5826e5a3f8a51f294a6d0
Cyrhina <cyrenemlumabas@gmail.com>
2023-09-04 10:10:57 +0800

    first push ni cy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { BrowserRouter as Router, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  // Create a client
+  return (
+    <Router>
+      <Routes>{/* <Route path={`*`} element={<PageNotFound />} /> */}</Routes>
+    </Router>
+  );
 }
 
 export default App;
