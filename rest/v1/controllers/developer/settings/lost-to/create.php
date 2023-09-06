3d371b3945b6fc8f7f77b0299fe63605d3ee5eb9
RoderickDM <RoderickDM>
2023-09-06 13:00:43 +0800

    lost to fname,lname

--- rest/v1/controllers/developer/settings/lost-to/create.php
+++ rest/v1/controllers/developer/settings/lost-to/create.php
@@ -12,6 +12,9 @@ if (array_key_exists("lostToId", $_GET)) {
 checkPayload($data);
 // get data
 $lostTo->lost_to_description = checkIndex($data, "lost_to_description");
+$lostTo->lost_to_first_name = checkIndex($data, "lost_to_first_name");
+$lostTo->lost_to_last_name = checkIndex($data, "lost_to_last_name");
+$lostTo->lost_to_name = checkIndex($data, "lost_to_name");
 
 $lostTo->lost_to_is_active = 1;
 $lostTo->lost_to_created_at = date("Y-m-d H:i:s");
