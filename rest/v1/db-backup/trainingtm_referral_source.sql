35f63c55153cd068358cf13465c2d8ac0f6ca670
Emmszhii <emmmanuelmanalo707@gmail.com>
2023-09-06 08:00:52 +0800

    update table

--- rest/v1/db-backup/trainingtm_referral_source.sql
+++ rest/v1/db-backup/trainingtm_referral_source.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2023 at 08:44 AM
+-- Generation Time: Sep 06, 2023 at 01:59 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -32,16 +32,21 @@ CREATE TABLE `trainingtm_referral_source` (
   `referral_source_name` varchar(100) NOT NULL,
   `referral_source_is_active` tinyint(4) NOT NULL,
   `referral_source_created_at` datetime NOT NULL,
-  `referral_source_update_at` datetime NOT NULL
+  `referral_source_update_at` datetime NOT NULL,
+  `referral_source_description` varchar(200) NOT NULL,
+  `referral_source_referral_type_id` varchar(20) NOT NULL,
+  `referral_source_department_id` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `trainingtm_referral_source`
 --
 
-INSERT INTO `trainingtm_referral_source` (`referral_source_aid`, `referral_source_name`, `referral_source_is_active`, `referral_source_created_at`, `referral_source_update_at`) VALUES
-(1, 'test', 1, '2023-09-05 14:19:44', '2023-09-05 14:40:17'),
-(3, 'test 321', 1, '2023-09-05 14:22:14', '2023-09-05 14:40:27');
+INSERT INTO `trainingtm_referral_source` (`referral_source_aid`, `referral_source_name`, `referral_source_is_active`, `referral_source_created_at`, `referral_source_update_at`, `referral_source_description`, `referral_source_referral_type_id`, `referral_source_department_id`) VALUES
+(1, 'test', 1, '2023-09-05 14:19:44', '2023-09-05 15:14:13', 'test', '', ''),
+(3, 'test 321', 1, '2023-09-05 14:22:14', '2023-09-05 15:14:10', 'test 321', '', ''),
+(4, 'gasdas', 1, '2023-09-05 15:07:11', '2023-09-05 15:14:17', 'gasdas', '', ''),
+(5, 'test0', 1, '2023-09-05 15:10:39', '2023-09-05 15:10:39', 'test0', '', '');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +66,7 @@ ALTER TABLE `trainingtm_referral_source`
 -- AUTO_INCREMENT for table `trainingtm_referral_source`
 --
 ALTER TABLE `trainingtm_referral_source`
-  MODIFY `referral_source_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `referral_source_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- rest/v1/db-backup/trainingtm_referral_type.sql
+++ rest/v1/db-backup/trainingtm_referral_type.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2023 at 03:49 AM
+-- Generation Time: Sep 06, 2023 at 01:59 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -32,22 +32,28 @@ CREATE TABLE `trainingtm_referral_type` (
   `referral_type_is_active` tinyint(4) NOT NULL,
   `referral_type_created_at` datetime NOT NULL,
   `referral_type_update_at` datetime NOT NULL,
-  `referral_type_name` varchar(100) NOT NULL
+  `referral_type_name` varchar(100) NOT NULL,
+  `referral_type_department_id` varchar(20) NOT NULL,
+  `referral_type_description` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `trainingtm_referral_type`
 --
 
-INSERT INTO `trainingtm_referral_type` (`referral_type_aid`, `referral_type_is_active`, `referral_type_created_at`, `referral_type_update_at`, `referral_type_name`) VALUES
-(1, 1, '2023-09-05 08:30:50', '2023-09-05 08:30:50', 'asdaa'),
-(2, 1, '2023-09-05 08:43:51', '2023-09-05 09:41:16', 'test'),
-(3, 0, '2023-09-05 09:00:29', '0000-00-00 00:00:00', 'asda'),
-(4, 1, '2023-09-05 09:01:02', '2023-09-05 09:01:02', 'asda'),
-(5, 1, '2023-09-05 09:03:50', '2023-09-05 09:03:50', 'asda'),
-(6, 1, '2023-09-05 09:04:05', '2023-09-05 09:04:05', 'asda'),
-(7, 0, '2023-09-05 09:04:08', '2023-09-05 09:22:59', 'sdsa'),
-(9, 1, '2023-09-05 09:04:21', '2023-09-05 09:04:21', 'asda');
+INSERT INTO `trainingtm_referral_type` (`referral_type_aid`, `referral_type_is_active`, `referral_type_created_at`, `referral_type_update_at`, `referral_type_name`, `referral_type_department_id`, `referral_type_description`) VALUES
+(1, 1, '2023-09-05 08:30:50', '2023-09-05 12:28:08', 'test33', '1', 'test33'),
+(2, 1, '2023-09-05 08:43:51', '2023-09-05 12:27:42', 'test10', '1', 'test11'),
+(4, 1, '2023-09-05 09:01:02', '2023-09-05 12:28:53', 'tes12', '3', 'tes12'),
+(5, 1, '2023-09-05 09:03:50', '2023-09-05 12:28:37', 'tees112', '3', 'tees112'),
+(6, 1, '2023-09-05 09:04:05', '2023-09-05 12:28:26', 't11', '1', 't11'),
+(9, 1, '2023-09-05 09:04:21', '2023-09-05 12:28:17', 'test11', '1', 'test11'),
+(11, 1, '2023-09-05 12:10:44', '2023-09-05 12:10:44', 'test2', '1', 'test2'),
+(12, 1, '2023-09-05 12:10:59', '2023-09-05 12:10:59', 'test3', '3', 'test3'),
+(13, 1, '2023-09-05 12:13:01', '2023-09-05 12:13:01', 'test4', '1', 'test4'),
+(14, 1, '2023-09-05 12:16:58', '2023-09-05 12:16:58', 'test6', '1', 'test6'),
+(15, 1, '2023-09-05 12:22:27', '2023-09-05 12:22:27', 'test7', '3', 'test7'),
+(16, 1, '2023-09-05 13:43:17', '2023-09-05 13:43:23', 'thea', '1', 'panget');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +73,7 @@ ALTER TABLE `trainingtm_referral_type`
 -- AUTO_INCREMENT for table `trainingtm_referral_type`
 --
 ALTER TABLE `trainingtm_referral_type`
-  MODIFY `referral_type_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `referral_type_aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
